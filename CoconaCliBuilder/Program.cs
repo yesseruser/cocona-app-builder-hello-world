d1a623dc97b34cadbb1dacd2b3dd31bc090bb231
yesseruser <yesseruseryt@gmail.com>
2024-02-17 23:51:45 +0100

    Use lambda instead of code block in RunAsync

--- CoconaCliBuilder/Program.cs
+++ CoconaCliBuilder/Program.cs
@@ -10,7 +10,5 @@ builder.Services.AddSingleton<IPrintService, PrintService>();
 using var app = builder.Build();
 
 await app.RunAsync(() =>
-{
     app.Services.GetService<IPrintService>()
-        ?.Print("Hello, World!");
-});
+    ?.Print("Hello, World!"));
